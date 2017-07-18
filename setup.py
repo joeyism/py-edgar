ef817f7aab07a477e647dbbdf9c1686cf43070af
joeyism <sham.joey@gmail.com>
2017-07-17 20:51:33 -0400

    Updated setup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
         name = 'edgar',
         packages = ['edgar'], # this must be the same as the name above
-        version = '0.2.0',
+        version = '0.3.0',
         description = 'Scrape data from SEC\'s EDGAR',
         author = 'Joey Sham',
         author_email = 'sham.joey@gmail.com',
         url = 'https://github.com/joeyism/py-edgar', # use the URL to the github repo
-        download_url = 'https://github.com/joeyism/py-edgar/archive/0.2.0.tar.gz',
+        download_url = 'https://github.com/joeyism/py-edgar/archive/0.3.0.tar.gz',
         keywords = ['edgar', 'sec'], 
         classifiers = [],
         )
