afe4ef80d188c0423ab261df652fc2502a327a49
joeyism <sham.joey@gmail.com>
2019-10-19 00:31:05 -0400

    Fixed import bug

--- edgar/__init__.py
+++ edgar/__init__.py
@@ -1,10 +1,10 @@
 from os.path import dirname, basename, isfile
 import glob
-from .edgar import *
-from .utils.txtml import TXTML
-from .company import Company
+from edgar.edgar import Edgar
+from edgar.utils.txtml import TXTML
+from edgar.company import Company
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 modules = glob.glob(dirname(__file__)+"/*.py")
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
