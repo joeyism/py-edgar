a03deb2a63e51ee130bd0c00b470053e67270325
joeyism <sham.joey@gmail.com>
2019-10-19 00:36:05 -0400

    updated version

--- edgar/__init__.py
+++ edgar/__init__.py
@@ -4,7 +4,7 @@ from .edgar import Edgar
 from .utils.txtml import TXTML
 from .company import Company
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 modules = glob.glob(dirname(__file__)+"/*.py")
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
