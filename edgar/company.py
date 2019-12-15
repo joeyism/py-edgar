6ba0979475f60156fa57c4ef65f84de21cca07e3
joeyism <sham.joey@gmail.com>
2019-12-15 16:37:24 -0500

    fixed bug

--- edgar/company.py
+++ edgar/company.py
@@ -13,10 +13,11 @@ class Company():
 
     def _get_company_info(self):
         page = html.fromstring(requests.get(self.url).content)
-        companyInfo = page.xpath("//div[@class='companyInfo']")[0]
-        indentInfo = companyInfo.getchildren()[1]
-        self.sic = indentInfo.getchildren()[1].text if len(indentInfo.getchildren()) > 2 else ""
-        self.us_state = indentInfo.getchildren()[3].text if len(indentInfo.getchildren()) > 4 else ""
+        companyInfo = page.xpath("//div[@class='companyInfo']")[0] if page.xpath("//div[@class='companyInfo']") else None
+        if companyInfo:
+          indentInfo = companyInfo.getchildren()[1]
+          self.sic = indentInfo.getchildren()[1].text if len(indentInfo.getchildren()) > 2 else ""
+          self.us_state = indentInfo.getchildren()[3].text if len(indentInfo.getchildren()) > 4 else ""
 
     def _get_filings_url(self, filing_type="", prior_to="", ownership="include", no_of_entries=100):
         url = self.url + "&type=" + filing_type + "&dateb=" + prior_to + "&owner=" +  ownership + "&count=" + str(no_of_entries)
