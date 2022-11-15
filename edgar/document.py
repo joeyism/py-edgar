ae749603b062a5d7f1d9ca1c8102c4f99b18b28b
eabase <52289379+eabase@users.noreply.github.com>
2022-11-15 13:36:37 +0200

    :diamonds: - maintenace update for required SEC headers (#29)
    * Added new required request headers for SEC EDGAR
    * Added UTF-8 header
    * Changed dependecy from broken fuzzwuzz to new rapidfuzz. Fixes #28
    * Fixes #24 (with user comment poor-mans patch for checking rate limits)
    * minor formatting adjustments for code readabiltiy
    
    Changes to be committed:
    	modified:   edgar/company.py
    	modified:   edgar/document.py
    	modified:   edgar/edgar.py
    	modified:   requirements.txt

--- edgar/document.py
+++ edgar/document.py
@@ -1,11 +1,25 @@
+# -*- coding: UTF-8 -*-
 import requests
 from lxml import html
+
+# The required SEC EDGAR request header
+SEC_HEADERS = {
+    'user-agent': 'Edgar oit@sec.gov',
+    'accept-encoding':  'gzip, deflate',
+    'host':  'www.sec.gov',
+    'referer': 'https://www.sec.gov/', 
+    'cache-control': 'no-cache', 
+    #'connection': 'close'
+    #'connection': 'keep-alive'
+}
+# Set new default requests header
+headers = SEC_HEADERS
 
 class Document:
 
   def __init__(self, url, timeout=10):
     self.url = url
-    self.text = requests.get(self.url, timeout=timeout).content
+    self.text = requests.get(self.url, timeout=timeout, headers=SEC_HEADERS).content
 
 class Documents(str):
 
@@ -14,13 +28,13 @@ class Documents(str):
 
   def __init__(self, url, timeout=10):
     self.url = url
-    page = requests.get(self.url, timeout=timeout)
+    page = requests.get(self.url, timeout=timeout, headers=SEC_HEADERS)
     tree = html.fromstring(page.content)
     content = tree.find_class("formContent")[0]
     info_head = self.__get_text_from_list__(content.find_class("infoHead"))
     info = self.__get_text_from_list__(content.find_class("info"))
     self.content = dict(zip(info_head, info))
-    self.element = html.fromstring(requests.get(self.url, timeout=timeout).content)
+    self.element = html.fromstring(requests.get(self.url, timeout=timeout, headers=SEC_HEADERS).content)
 
   def __repr__(self):
     return str(self.__dict__)
